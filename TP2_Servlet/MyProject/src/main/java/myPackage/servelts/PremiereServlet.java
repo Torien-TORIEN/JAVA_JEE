ce4287cfe4741eeb14287cc7ea74d81cc2ee29d3
Torien <torien1227@gmail.com>
2024-04-07 12:04:25 +0200

    TP2

--- TP2_Servlet/MyProject/src/main/java/myPackage/servelts/PremiereServlet.java
+++ TP2_Servlet/MyProject/src/main/java/myPackage/servelts/PremiereServlet.java
@@ -1,6 +1,8 @@
 package myPackage.servelts;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,17 @@ public class PremiereServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>"
+				+ "<html>"
+				+ "<head>"
+				+ "<title>Example</title>"
+				+ "</head>"
+				+ "<body>"
+				+ "<p> C'est ma première servlet !</p>"
+				+ "</body>"
+				+ "</html>");
 	}
 
 	/**
