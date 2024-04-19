cebbd5965a257194592d921b0404234fe6896d0c
Torien <torien1227@gmail.com>
2024-04-19 04:29:17 +0200

    TP4 exo3

--- jpaTest1/src/jpaPack/jpaMain.java
+++ jpaTest1/src/jpaPack/jpaMain.java
@@ -1,24 +1,14 @@
 package jpaPack;
+import mypackage.Nation;
+
+
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
-import myPackage.Nation;
-
 public class jpaMain {
-	
-	public static void  lister(EntityManager entityManager  ) {
-		Query q = entityManager.createQuery( "from Nation" , Nation.class );
-		List<Nation> nations ;
-		nations = q.getResultList();
-		
-		for (Nation n : nations) {
-			System.out.println( n.toString() );
-		}
-	}
-	
 	public static void main(String[] args) throws Exception {
 		EntityManagerFactory emf ;
 		emf = Persistence.createEntityManagerFactory("UniteUnionEurope");
@@ -37,10 +27,6 @@ public class jpaMain {
 		entityManager.persist(nation); // Ajout de la nouvelle Nation à la base de données
 		entityManager.getTransaction().commit(); // Validation de la transaction
 		
-		// Afficher la liste
-		System.out.println("\nApres l'ajout :");
-		lister(entityManager);
-		
 		
 		// Mise à jour d'un enregistrement
 		entityManager.getTransaction().begin(); // Début de la transaction
@@ -48,20 +34,12 @@ public class jpaMain {
 		nationToUpdate.setNom("Bulgarie"); // Modification de l'attribut
 		entityManager.getTransaction().commit(); // Validation de la transaction
 		
-		// Afficher la liste
-		System.out.println("\nApres la modification :");
-		lister(entityManager);
-		
 		
 		//Suppression d'un enregistrement
 		entityManager.getTransaction().begin(); // Début de la transaction
 		Nation nationToDelete = entityManager.find(Nation.class, 7); // Récupération de l'entité à supprimer
 		entityManager.remove(nationToDelete); // Suppression de l'entité de la base de données
 		entityManager.getTransaction().commit(); // Validation de la transaction
-		
-		// Afficher la liste
-		System.out.println("\nApres la suppression :");
-		lister(entityManager);
 		
 		
 	}
