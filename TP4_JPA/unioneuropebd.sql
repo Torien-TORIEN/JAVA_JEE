abd8d4d83e810439236a8b94755ec7a8c445b4c0
Torien <torien1227@gmail.com>
2024-04-19 02:01:35 +0200

    TP4 exo2

---
+++ TP4_JPA/unioneuropebd.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 19 avr. 2024 à 01:58
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `unioneuropebd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `nations`
+--
+
+CREATE TABLE `nations` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `population` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `nations`
+--
+
+INSERT INTO `nations` (`id`, `nom`, `population`) VALUES
+(1, 'France', 67000000),
+(2, 'Allemagne', 83000000),
+(3, 'Espagne', 47000000),
+(4, 'Italie', 60000000),
+(5, 'Royaume-Uni', 66000000);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `nations`
+--
+ALTER TABLE `nations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `nations`
+--
+ALTER TABLE `nations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
